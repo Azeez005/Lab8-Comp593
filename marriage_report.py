819980351e06d3c3ba8e1d3d06d85fd2710e72fd
Azeez005 <156537132+Azeez005@users.noreply.github.com>
2024-07-26 13:41:28 -0400

    .

---
+++ marriage_report.py
@@ -0,0 +1,66 @@
+"""
+Description:
+ Generates a CSV reports containing all married couples in
+ the Social Network database.
+
+Usage:
+ python marriage_report.py
+"""
+import os
+import sqlite3
+from create_relationships import db_path, script_dir
+
+def main():
+    # Query DB for list of married couples
+    married_couples = get_married_couples()
+
+    # Save all married couples to CSV file
+#    csv_path = os.path.join(script_dir, 'married_couples.csv')
+    save_married_couples_csv(married_couples, csv_path)
+
+def get_married_couples():
+    """Queries the Social Network database for all married couples.
+
+    Returns:
+        list: (name1, name2, start_date) of married couples 
+    """
+    # TODO: 
+all_relationships_query = """
+    SELECT person1.name, person2.name, start_date, type FROM relationships
+    JOIN people person1 ON person1_id = person1.id
+    JOIN people person2 ON person2_id = person2.id;
+"""
+    # Hint: See example code in lab instructions entitled "Get a List of Relationships"
+con = sqlite3.connect(db_path)
+cur.execute(all_relationships_query)
+all_relationships = cur.fetchall()
+con.close()
+# Print sentences describing each relationship
+#for person1, person2, start_date, type in all_relationships:
+#print(f'{person1} has been a {type} of {person2} since {start_date}.')
+
+
+def save_married_couples_csv(married_couples, csv_path):
+    """Saves list of married couples to a CSV file, including both people's 
+    names and their wedding anniversary date  
+
+    Args:
+        married_couples (list): (name1, name2, start_date) of married couples
+        csv_path (str): Path of CSV file
+"""
+    # TODO: 
+"""Saves name and age of all people in provided list
+
+Args:
+    name_and_age_list (list): (name, age) of people
+    csv_path (str): Path of CSV file
+"""
+with open(csv_path, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(['Name', 'type'])
+    csv_writer.writerows(name_and_type_list)
+
+    # Hint: We did this in Lab 7.
+
+if __name__ == '__main__':
+   main()
